8ca1432309d1f27768592df76153f63ea0539b03
Martyn Rushton <martyn.e.rushton@gmail.com>
2020-05-26 11:46:32 +0200

    working

--- api/random.ts
+++ api/random.ts
@@ -1,7 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node';
-import getRandomNumber from '~utils/randomNumber';
+import getRandomNumber from '../utils/randomNumber';
 
 export default (req: NowRequest, res: NowResponse) => {
+    console.log('random')
     res.status(200).json({
       name: 'John',
       email: 'john@example.com',
